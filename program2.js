b9e47f806fdc85e4d12aa6af7ac30eb0fa7aa031
prince6264 <pandeyprince7980@gmail.com>
2024-09-30 12:20:25 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Mapping of Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    const length = s.length;
+    
+    for (let i = 0; i < length; i++) {
+        const value = romanMap[s[i]];
+        
+        // If this is not the last character and the next character is larger, we subtract
+        if (i < length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            total -= value;  // Subtract if the next numeral is larger
+        } else {
+            total += value;  // Otherwise, add the value
+        }
+    }
+    
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
