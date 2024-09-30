73a71908d428848a5d82d941862970329729932c
prince6264 <pandeyprince7980@gmail.com>
2024-09-30 12:43:26 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,25 +1,31 @@
-def roman_to_int(s: str) -> int:
-    # Mapping of Roman numerals to their integer values
-    roman_map = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
-    total = 0
-    length = len(s)
-    
-    for i in range(length):
-        value = roman_map[s[i]]
-        
-        # If this is not the last character and the next character is larger, we subtract
-        if i < length - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
-            total -= value
-        else:
-            total += value
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    // Mapping of closing brackets to opening brackets
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    const stack = [];
+
+    for (const char of s) {
+        if (bracketMap[char]) { 
             
-    return total
+            if (stack.length > 0 && stack[stack.length - 1] === bracketMap[char]) {
+                stack.pop(); 
+                return false; 
+            }
+        } else {
+           
+            stack.push(char);
+        }
+    }
+
+    
+    return stack.length === 0;
+};
+
+module.exports = { isValid };
