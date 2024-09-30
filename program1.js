8234f3614e093dd20ab71f105a7914e5ee5deff4
prince6264 <pandeyprince7980@gmail.com>
2024-09-30 12:45:57 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,5 @@
 /**
- * @param {string} 
+ * @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
@@ -12,19 +12,20 @@ var isValid = function(s) {
     const stack = [];
 
     for (const char of s) {
-        if (bracketMap[char]) { 
-            
+        if (bracketMap[char]) {  // If it's a closing bracket
+            // Check if the stack is not empty and matches the opening bracket
             if (stack.length > 0 && stack[stack.length - 1] === bracketMap[char]) {
-                stack.pop(); 
-                return false; 
+                stack.pop();  // Valid match found, pop the opening bracket
+            } else {
+                return false;  // Invalid closing bracket
             }
         } else {
-           
+            // If it's an opening bracket, push it onto the stack
             stack.push(char);
         }
     }
 
-    
+    // Stack should be empty if valid
     return stack.length === 0;
 };
 
